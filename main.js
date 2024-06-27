3583bff9598a2b7e2367179a377c859d2c67ab6b
ScriptSmith <ScriptSmith@users.noreply.github.com>
2024-06-27 21:46:17 +1000

    Add light/dark icons with options page

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ browser.browserAction.onClicked.addListener(() =>
       .query({ currentWindow: true, discarded: true })
       .then((tabs) => tabs.map((tab) => browser.tabs.remove(tab.id))),
   );
+
+  browser.storage.local.onChanged.addListener((changes) => {
+    const iconColor = changes["iconColor"];
+    if (iconColor) {
+      browser.browserAction.setIcon({
+        path: {
+          48: `icons/trash-48-${iconColor.newValue}.png`,
+        },
+      });
+    }
+  });
