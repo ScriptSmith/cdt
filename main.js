8eba9c4f12c632b03fe1e0f63eadd9b6c2011f57
ScriptSmith <ScriptSmith@users.noreply.github.com>
2024-07-19 22:52:49 +1000

    Fix persistence

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
-browser.browserAction.onClicked.addListener(() =>
-    browser.tabs
-      .query({ currentWindow: true, discarded: true })
-      .then((tabs) => tabs.map((tab) => browser.tabs.remove(tab.id))),
-  );
+const DEFAULT_COLOR = "dark";
 
-  browser.storage.local.onChanged.addListener((changes) => {
-    const iconColor = changes["iconColor"];
-    if (iconColor) {
-      browser.browserAction.setIcon({
-        path: {
-          48: `icons/trash-48-${iconColor.newValue}.png`,
-        },
-      });
-    }
+browser.browserAction.onClicked.addListener(() =>
+  browser.tabs
+    .query({ currentWindow: true, discarded: true })
+    .then((tabs) => tabs.map((tab) => browser.tabs.remove(tab.id))),
+);
+
+const setIcon = (color) =>
+  browser.browserAction.setIcon({
+    path: {
+      48: `icons/trash-48-${color}.png`,
+    },
   });
+
+browser.storage.local
+  .get("iconColor")
+  .then(({ iconColor }) => setIcon(iconColor));
+
+browser.storage.local.onChanged.addListener((changes) => {
+  const iconColor = changes["iconColor"];
+  if (iconColor) {
+    setIcon(iconColor.newValue);
+  }
+});
